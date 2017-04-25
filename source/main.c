c2391614da77a01765e9d246184f66505e155ecf
xujwoo <xujianwei201314@icloud.com>
2017-04-25 22:29:03 +0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,9 +1,4 @@
-/**
- * This software is copyrighted by Bosch Connected Devices and Solutions GmbH, 2015. 
- * The use of this software is subject to the XDK SDK EULA
- */
 
-/* system header files */
 #include <stdint.h>
 #include <stdio.h>
 
